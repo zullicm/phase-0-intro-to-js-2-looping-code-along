811da4102c8bb9663f73bfc7ee8f4d9075f75b59
Christopher Marzulli <chriszulli15@gmail.com>
2022-04-28 12:51:24 -0400

    commit tree d34618caa96b1e6a86bfb2782b9c824b289f8096

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    let cardMessage = []
+    for (let i = 0; i < names.length; i++){
+        cardMessage[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    }
+    return cardMessage
+}
+
+function countDown(){
+    let num = 10
+    while (num >= 0){
+        console.log(num--)
+    }
+}
